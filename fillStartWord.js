a3f074c6b41f59cee3ecb049264fa349299bac01
Siripoom <siripoom.kusonsong@kmutt.mail.ac.th>
2022-09-01 14:04:52 +0700

    Code : Update

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,8 +1,9 @@
-const { template } = require('@babel/core')
+const { template } = require("@babel/core");
 
 function fillStartWord(startWord, word) {
-  //code here
-  if (word == null) return undefined;
-  return (startWord === word.slice(0,startWord.length) ? word : startWord);
+  if (word == null || undefined) return undefined;
+  else if (startWord === word.slice(0, startWord.length)) {
+    return word;
+  } else return startWord + word;
 }
-module.exports = fillStartWord
+module.exports = fillStartWord;

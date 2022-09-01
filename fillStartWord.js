cd05db75256c818fab3cb352a06f3ee0b0cab093
Siripoom <siripoom.kusonsong@kmutt.mail.ac.th>
2022-09-01 10:34:59 +0700

    firstCommit

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,6 +2,7 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+  if (word == null) return undefined;
+  return (startWord === word.slice(0,startWord.length) ? word : startWord);
 }
 module.exports = fillStartWord
-// TestCommit
